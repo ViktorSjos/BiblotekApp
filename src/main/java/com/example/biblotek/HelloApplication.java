7f533f393b6c7eeab2a8e51e2aa3441d0b985c9a
ViktorS <viksjs-9@student.ltu.se>
2023-05-12 00:08:15 +0200

    te

--- src/main/java/com/example/biblotek/HelloApplication.java
+++ src/main/java/com/example/biblotek/HelloApplication.java
@@ -5,16 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws IOException, SQLException, ClassNotFoundException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+        DatabaseConnection databaseConnection = new DatabaseConnection();
+        databaseConnection.getConnection();
     }
+
+
 
     public static void main(String[] args) {
         launch();
